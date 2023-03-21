79f9253a01083e569a72cf25d5854fd946b42f98
Mbalenhle09 <mahlabangane.co.za@gmail.com>
2023-03-21 16:46:18 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print first 50 fib sequence
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	long int next;
+	long int first = 1;
+	long int second = 2;
+	long int count;
+
+	for (count = 1; count <= 50; ++count)
+	{
+		if (count != 50)
+		{
+			printf("%ld, ", first);
+		}
+		else
+		{
+			printf("%ld\n", first);
+		}
+		next = first + second;
+		first = second;
+		second = next;
+	}
+	return (0);
+}
