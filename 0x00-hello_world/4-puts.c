90346a65f67139dfb07cc4f8d75838c88643ce36
MbalenhleM09 <mahlabangane.co.za@gmail.com>
2023-03-22 13:25:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,14 +3,13 @@
 
 /**
  * main - prints "programming is like building a multilingual
- * puzzle, followed by a newline."
- * return 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
 int main(void)
-
 {
-	puts("\"programming is like building a multilingual puzzle";
+	puts("\"programming is like building a multilingual puzzle");
 
 	return (0);
 }
