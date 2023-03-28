45c5df1fbd523ada00dac6102128228af426f02a
MbalenhleM09 <mahlabangane.co.za@gmail.com>
2023-03-28 16:32:11 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Draws a diagonal lires according piraneter
+ * @n: The number of times to print diagonal lires
+ * Return: empty
+ */
+
+void print_diagonal(int n)
+
+{
+	int x, y
+
+	if (n <= 0)
+	{
+	_putchar('\n');
+	}
+	else
+	{
+	for (x = 0, x < n, x++)
+	{
+	for (y = 0, y < n, y++)
+	{
+	_putchar(32);
+	}
+	_putchar(92);
+	_putchar('\n');
+	}
+	}
+}
