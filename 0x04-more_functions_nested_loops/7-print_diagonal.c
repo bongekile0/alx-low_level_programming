c767869832d813a2bd7b1e7616325b18f5cb1fcb
MbalenhleM09 <mahlabangane.co.za@gmail.com>
2023-04-03 12:58:21 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -11,20 +11,21 @@ void print_diagonal(int n)
 {
 	int x, y
 
-	if (n <= 0)
+	if (n < = 0)
 	{
 	_putchar('\n');
 	}
 	else
-
-	for (x = 0, x < n, x++)
-
-	for (y = 0, y < n, y++)
+	{
+	for (x = 0; x < n; x++)
+	{
+	for (y = 0; y < n; y++)
 	{
 	_putchar(32);
 	}
 	_putchar(92);
-	{
 	_putchar('\n');
 	}
+	}
+
 }
